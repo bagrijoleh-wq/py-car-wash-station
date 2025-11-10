e1bf83add7a58762c04a634372d0c54111c5a203
Oleg <bagrijoleh@gmail.com>
2025-11-10 23:23:30 +0200

    fixed issue (add round income)

--- app/main.py
+++ app/main.py
@@ -17,14 +17,13 @@ class CarWashStation:
             if self.clean_power > car.clean_mark:
                 income += self.calculate_washing_price(car)
                 car.clean_mark = self.clean_power
-        return income
+        return round(income, 1)
 
     def calculate_washing_price(self, car: Car):
         if self.clean_power > car.clean_mark:
             washing_price = car.comfort_class * (self.clean_power - car.clean_mark) * self.average_rating / self.distance_from_city_center
             return  round(washing_price, 1)
-        else:
-            return 0
+        return 0
 
     def wash_single_car(self, car: Car):
         if self.clean_power > car.clean_mark:
